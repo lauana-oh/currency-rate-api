690eae2eb17c7d9c221c255713e1e0c34f526518
lauana <lauana.oliveira@evertecinc.com>
2021-11-29 00:20:08 -0500

    feat: added dracma variables to .env.example

--- config/dracma.php
+++ config/dracma.php
@@ -6,7 +6,7 @@ return [
     'drivers' => [
         'currency_layer' => [
             'url' => env('DRACMA_CURRENCY_LAYER_URL', 'http://api.currencylayer.com'),
-            'access_key' => env('DRACMA_CURRENCY_LAYER_ACCESS_KEY', 'c32fe9638fbcfc9de26089522d46e01e'),
+            'access_key' => env('DRACMA_CURRENCY_LAYER_ACCESS_KEY'),
         ],
     ],
 
